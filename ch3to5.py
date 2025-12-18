c64504105e123e6163d9859732da17df6de049cc
Aryan Bharti <aryanbharti165@gmail.com>
2025-12-19 01:58:18 +0530

    Added code for q3.3

--- ch3to5.py
+++ ch3to5.py
@@ -4,14 +4,42 @@
 # You should use input to read a string and float() to convert the string to a number. 
 # Do not worry about error checking the user input - assume the user types numbers properly.
 
-hrs = input("Enter Hours:")
-h = float(hrs)
-rate = input("Enter rate:")
-r = float(rate)
+#h = float(hrs)
+#rate = input("Enter rate:")
+#hrs = input("Enter Hours:")
+#r = float(rate)
 
-if h < 40:
-    gross_pay = h * r
+#if h < 40:
+#    gross_pay = h * r
+#else:
+#    gross_pay = 40 * r + (h-40) * r * 1.5
+
+#print(gross_pay)
+
+# *****************************************************************
+
+
+#3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. 
+# If the score is between 0.0 and 1.0, print a grade using the following table:
+#Score Grade
+#>= 0.9 A
+#>= 0.8 B
+#>= 0.7 C
+#>= 0.6 D
+#< 0.6 F
+#If the user enters a value out of range, print a suitable error message and exit. 
+# For the test, enter a score of 0.85.
+
+score = input("Enter Score: ")
+s = float(score)
+
+if s >= 0.9:
+    print("A")
+elif s >= 0.8:
+    print("B")
+elif s >= 0.7:
+    print("C")
+elif s >= 0.6:
+    print("D")
 else:
-    gross_pay = 40 * r + (h-40) * r * 1.5
-
-print(gross_pay)
+    print("F")
