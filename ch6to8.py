dc2cac60f54212cd15efc5387dc929685fd171da
Aryan Bharti <aryanbharti165@gmail.com>
2025-12-21 18:09:57 +0530

    Added code for q8.4

--- ch6to8.py
+++ ch6to8.py
@@ -48,3 +48,24 @@
 # print("Average spam confidence:", average)
 
 # *****************************************************************
+
+# 8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of 
+# words using the split() method. The program should build a list of words. For each word on each line 
+# check to see if the word is already in the list and if not append it to the list. 
+# When the program completes, sort and print the resulting words in python sort() order as shown in 
+# the desired output.
+
+# fname = input("Enter file name: ")
+# fh = open(fname)
+
+# lst = []
+
+# for line in fh:
+#     for word in line.split():
+#         if not word in lst:
+#             lst.append(word)
+# lst.sort()
+# print(lst)
+
+# *****************************************************************
+
