b9dd0578ff1ec0369a686e85c6bf70d39a37226a
Aryan Bharti <aryanbharti165@gmail.com>
2025-12-21 17:23:42 +0530

    Added q7.2

--- ch6to8.py
+++ ch6to8.py
@@ -7,6 +7,8 @@
 # num = float(text[pos:pos+6])
 # print(num)
 
+# *****************************************************************
+
 # 7.1 Write a program that prompts for a file name, then opens that file and reads through the file, 
 # and print the contents of the file in upper case. Use the file words.txt to produce the output below.
 
@@ -19,3 +21,30 @@
 
 # for line in fh:
 #     print(line.rstrip().upper())
+
+# *****************************************************************
+
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, 
+# looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and 
+# compute the average of those values and produce an output as shown below. Do not use the sum() 
+# function or a variable named sum in your solution.
+# Use the file name mbox-short.txt as the file name
+
+# fname = input("Enter file name: ")
+# fh = open(fname)
+# average = 0
+# count = 0
+
+# for line in fh:
+#     if not line.startswith("X-DSPAM-Confidence:"):
+#         continue
+#     count += 1
+#     pos = line.find('0')
+#     average += float(line[pos:pos+6])
+
+# average /= count
+# print("Average spam confidence:", average)
+
+# *****************************************************************
